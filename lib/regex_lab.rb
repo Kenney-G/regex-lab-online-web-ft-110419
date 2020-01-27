81be78c0d164902655aabc4cb359f219c13380f4
Ken Goad <goadke@g.cofc.edu>
2020-01-27 20:43:13 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,19 @@
-def starts_with_a_vowel?(word)
+# Working with Regular expressions
+#   #starts_with_a_vowel?
+#     returns true for words starting with a vowel (FAILED - 1)
+#     returns false for words starting with a consonant (FAILED - 2)
+#   #words_starting_with_un_and_ending_with_ing
+#     returns an array with the words starting with 'un' and ending with 'ing' (FAILED - 3)
+#   #words_five_letters_long
+#     returns an array of words that are five letters long (FAILED - 4)
+#   #first_word_capitalized_and_ends_with_punctuation?
+#     Returns true for text starting with a capital letter and ending with puncutation (FAILED - 5)
+#     Returns false for text starting with an uncapitalized letter but ending with puncutation (FAILED - 6)
+#     Returns false for text starting with a capital letter but ending without puncutation (FAILED - 7)
+#     Returns false for text starting without a capital letter and ending withoutpuncutation (FAILED - 8)
 
+def starts_with_a_vowel?(word)
+word.match(/^[aeiouAEIOU]\w+/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
