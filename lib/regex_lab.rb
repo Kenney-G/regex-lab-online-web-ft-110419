5febccd6ff2856de9d824417851404ab098961fd
Ken Goad <goadke@g.cofc.edu>
2020-01-27 20:53:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -29,5 +29,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/^[2-9]\d{2}-\d{3}-\d{4}$/)
+  phone.match(/^(1?(-?\d{3})-?)?(\d{3})(-?\d{4})$/)
 end
